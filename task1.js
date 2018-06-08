ebe67cb857714b001f14bc4ebfb12308a19f9561
Soslan <soslan_kunduhov@mail.ru>
2018-06-08 17:39:28 +0300

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,9 +1,10 @@
-var x1 = +process.argv[2];
-var y1 = +process.argv[3];
-var x2 = +process.argv[4];
-var y2 = +process.argv[5];
-if((x1>=-10000)&&(x1<=10000)&&(x2>=-10000)&&(x2<=10000)&&(y1>=-10000)&&(y1<=10000)&&(y2>=-10000)&&(y2<=10000)){
-	var resX = x1+x2;
-	var resY = y1+y2;
-	process.stdout.write(resX,resY);  
+var n = process.argv[2];
+var e = process.argv[3];
+var result = 0;
+if((n>=1)&&(e<=100)){
+	for(var i=1;i<=n;i++){
+		result += i**e;
+		
+	}
 }
+console.log(result)
